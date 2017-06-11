2193957a687a090567a4a0f35beef67fc2a015d3
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2017-06-11 23:55:38 +0200

    External configuration

--- src/main/scala/petclinic/mysql/package.scala
+++ src/main/scala/petclinic/mysql/package.scala
@@ -1,18 +1,23 @@
 package petclinic
 
 import cats.data.EitherT
+import com.typesafe.config.ConfigFactory
 import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.{ Failure, Success }
+import scala.util.{ Failure, Success, Try }
 import scala.util.control.NonFatal
+import scala.sys.process._
 import java.sql.{ Connection, DriverManager }
 
 package object mysql {
 
-  final val Ip       = "localhost"
+  final val Config = ConfigFactory.load()
+
+  final val Ip       =
+    Try("docker-machine ip".!!).orElse(Try(Config.getString("mysql.ip"))).getOrElse("localhost")
   final val Driver   = "com.mysql.jdbc.Driver"
   final val Url      = s"jdbc:mysql://$Ip/petclinic"
-  final val username = "root"
-  final val password = "root"
+  final val username = Try(Config.getString("mysql.username")).getOrElse("root")
+  final val password = Try(Config.getString("mysql.password")).getOrElse("root")
 
   def withConnection[A](f: Connection => Either[PetClinicError, A])(
       implicit ec: ExecutionContext): Response[A] =
